d8928eb0f51dd91c679e9d1c4e49ccb6e4418738
RouxJr <anathi.roux@gmail.com>
2018-06-25 21:00:33 +0200

    Changed sql stuff

--- SQL_scrips/conect(1).sql
+++ SQL_scrips/conect(1).sql
@@ -2,10 +2,10 @@
 -- version 4.7.4
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Jun 25, 2018 at 08:50 PM
--- Server version: 10.1.28-MariaDB
--- PHP Version: 7.1.10
+-- Host: 127.0.0.1:3306
+-- Generation Time: Jun 25, 2018 at 06:43 PM
+-- Server version: 5.7.19
+-- PHP Version: 5.6.31
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -30,12 +30,14 @@ USE `conect`;
 -- Table structure for table `activity`
 --
 
-CREATE TABLE `activity` (
+DROP TABLE IF EXISTS `activity`;
+CREATE TABLE IF NOT EXISTS `activity` (
   `EveID` int(11) NOT NULL,
-  `EveActID` int(11) NOT NULL,
+  `EveActID` int(11) NOT NULL AUTO_INCREMENT,
   `Title` text NOT NULL,
-  `Descr` text NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `Descr` text NOT NULL,
+  UNIQUE KEY `EveActID` (`EveActID`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -43,16 +45,18 @@ CREATE TABLE `activity` (
 -- Table structure for table `business`
 --
 
-CREATE TABLE `business` (
-  `BusID` int(11) NOT NULL,
+DROP TABLE IF EXISTS `business`;
+CREATE TABLE IF NOT EXISTS `business` (
+  `BusID` int(11) NOT NULL AUTO_INCREMENT,
   `BusName` varchar(50) DEFAULT NULL,
   `BusLogo` varchar(100) DEFAULT NULL,
   `BusSlogan` varchar(100) DEFAULT NULL,
   `VAT` float DEFAULT NULL,
   `BusAddressID` int(11) DEFAULT NULL,
   `BusAboutUs` text,
-  `BusDateFound` date DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `BusDateFound` date DEFAULT NULL,
+  PRIMARY KEY (`BusID`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -60,14 +64,16 @@ CREATE TABLE `business` (
 -- Table structure for table `event`
 --
 
-CREATE TABLE `event` (
-  `EveID` int(11) NOT NULL,
+DROP TABLE IF EXISTS `event`;
+CREATE TABLE IF NOT EXISTS `event` (
+  `EveID` int(11) NOT NULL AUTO_INCREMENT,
   `EveName` varchar(100) DEFAULT NULL,
   `EveStartDate` date DEFAULT NULL,
   `EveAddress` text,
   `EveDescription` text,
-  `EveEndDate` date NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `EveEndDate` date NOT NULL,
+  PRIMARY KEY (`EveID`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -75,7 +81,8 @@ CREATE TABLE `event` (
 -- Table structure for table `event_ticket`
 --
 
-CREATE TABLE `event_ticket` (
+DROP TABLE IF EXISTS `event_ticket`;
+CREATE TABLE IF NOT EXISTS `event_ticket` (
   `EveID` int(11) NOT NULL,
   `TicID` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
@@ -86,12 +93,16 @@ CREATE TABLE `event_ticket` (
 -- Table structure for table `gallery`
 --
 
-CREATE TABLE `gallery` (
-  `GalID` int(11) NOT NULL,
+DROP TABLE IF EXISTS `gallery`;
+CREATE TABLE IF NOT EXISTS `gallery` (
+  `GalID` int(11) NOT NULL AUTO_INCREMENT,
   `GalPic` longblob,
-  `GalDate` date DEFAULT NULL,
-  `GalDescrip` varchar(100) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `GalDate` year(4) DEFAULT NULL,
+  `GalDescrip` varchar(100) DEFAULT NULL,
+  `GalTime` time(6) DEFAULT NULL,
+  PRIMARY KEY (`GalID`),
+  UNIQUE KEY `GalTime` (`GalTime`)
+) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -99,12 +110,14 @@ CREATE TABLE `gallery` (
 -- Table structure for table `item`
 --
 
-CREATE TABLE `item` (
+DROP TABLE IF EXISTS `item`;
+CREATE TABLE IF NOT EXISTS `item` (
   `ItemID` int(11) NOT NULL,
   `IteName` varchar(100) NOT NULL,
   `IteDescription` text,
   `IteQty` int(11) NOT NULL,
-  `ItePrice` double NOT NULL
+  `ItePrice` double NOT NULL,
+  PRIMARY KEY (`ItemID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -113,12 +126,14 @@ CREATE TABLE `item` (
 -- Table structure for table `pictures`
 --
 
-CREATE TABLE `pictures` (
-  `PicID` int(11) NOT NULL,
+DROP TABLE IF EXISTS `pictures`;
+CREATE TABLE IF NOT EXISTS `pictures` (
+  `PicID` int(11) NOT NULL AUTO_INCREMENT,
   `PicPicture` longblob,
   `PicDescription` varchar(100) DEFAULT NULL,
-  `PicOther` int(11) DEFAULT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+  `PicOther` int(11) DEFAULT NULL,
+  PRIMARY KEY (`PicID`)
+) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -126,12 +141,16 @@ CREATE TABLE `pictures` (
 -- Table structure for table `sponsor`
 --
 
-CREATE TABLE `sponsor` (
-  `SpoID` int(11) NOT NULL,
+DROP TABLE IF EXISTS `sponsor`;
+CREATE TABLE IF NOT EXISTS `sponsor` (
+  `SpoID` int(11) NOT NULL AUTO_INCREMENT,
   `SpoName` varchar(100) DEFAULT NULL,
   `SpoWebsite` varchar(100) DEFAULT NULL,
-  `SpoPicture` longblob
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `SpoPicture` longblob,
+  PRIMARY KEY (`SpoID`),
+  UNIQUE KEY `SpoWebsite` (`SpoWebsite`),
+  KEY `SpoID` (`SpoID`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -139,15 +158,17 @@ CREATE TABLE `sponsor` (
 -- Table structure for table `ticket`
 --
 
-CREATE TABLE `ticket` (
-  `TicID` int(11) NOT NULL,
+DROP TABLE IF EXISTS `ticket`;
+CREATE TABLE IF NOT EXISTS `ticket` (
+  `TicID` int(11) NOT NULL AUTO_INCREMENT,
   `TicPriceWeekendPass` double DEFAULT NULL,
   `TicDescription` text,
   `TicType` int(11) DEFAULT NULL,
   `TicPriceNormalPass` double DEFAULT NULL,
   `TicPriceSpecialPass` double DEFAULT NULL,
-  `EveID` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `EveID` int(11) DEFAULT NULL,
+  PRIMARY KEY (`TicID`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -155,9 +176,11 @@ CREATE TABLE `ticket` (
 -- Table structure for table `ticket_type`
 --
 
-CREATE TABLE `ticket_type` (
+DROP TABLE IF EXISTS `ticket_type`;
+CREATE TABLE IF NOT EXISTS `ticket_type` (
   `TicTypID` int(11) NOT NULL,
-  `TicTypName` varchar(50) NOT NULL
+  `TicTypName` varchar(50) NOT NULL,
+  PRIMARY KEY (`TicTypID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -166,8 +189,9 @@ CREATE TABLE `ticket_type` (
 -- Table structure for table `vendor`
 --
 
-CREATE TABLE `vendor` (
-  `VenID` int(11) NOT NULL,
+DROP TABLE IF EXISTS `vendor`;
+CREATE TABLE IF NOT EXISTS `vendor` (
+  `VenID` int(11) NOT NULL AUTO_INCREMENT,
   `VenName` varchar(100) DEFAULT NULL,
   `VenFacebook` varchar(100) DEFAULT NULL,
   `VenTwitter` varchar(100) DEFAULT NULL,
@@ -175,127 +199,10 @@ CREATE TABLE `vendor` (
   `VenWebsite` varchar(100) DEFAULT NULL,
   `VenDescription` text,
   `ItemID` int(11) DEFAULT NULL,
-  `VenPicture` longblob
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `activity`
---
-ALTER TABLE `activity`
-  ADD UNIQUE KEY `EveActID` (`EveActID`);
-
---
--- Indexes for table `business`
---
-ALTER TABLE `business`
-  ADD PRIMARY KEY (`BusID`);
-
---
--- Indexes for table `event`
---
-ALTER TABLE `event`
-  ADD PRIMARY KEY (`EveID`);
-
---
--- Indexes for table `gallery`
---
-ALTER TABLE `gallery`
-  ADD PRIMARY KEY (`GalID`);
-
---
--- Indexes for table `item`
---
-ALTER TABLE `item`
-  ADD PRIMARY KEY (`ItemID`);
-
---
--- Indexes for table `pictures`
---
-ALTER TABLE `pictures`
-  ADD PRIMARY KEY (`PicID`);
-
---
--- Indexes for table `sponsor`
---
-ALTER TABLE `sponsor`
-  ADD PRIMARY KEY (`SpoID`),
-  ADD UNIQUE KEY `SpoWebsite` (`SpoWebsite`),
-  ADD KEY `SpoID` (`SpoID`);
-
---
--- Indexes for table `ticket`
---
-ALTER TABLE `ticket`
-  ADD PRIMARY KEY (`TicID`);
-
---
--- Indexes for table `ticket_type`
---
-ALTER TABLE `ticket_type`
-  ADD PRIMARY KEY (`TicTypID`);
-
---
--- Indexes for table `vendor`
---
-ALTER TABLE `vendor`
-  ADD PRIMARY KEY (`VenID`),
-  ADD UNIQUE KEY `VenName` (`VenName`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `activity`
---
-ALTER TABLE `activity`
-  MODIFY `EveActID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-
---
--- AUTO_INCREMENT for table `business`
---
-ALTER TABLE `business`
-  MODIFY `BusID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-
---
--- AUTO_INCREMENT for table `event`
---
-ALTER TABLE `event`
-  MODIFY `EveID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
-
---
--- AUTO_INCREMENT for table `gallery`
---
-ALTER TABLE `gallery`
-  MODIFY `GalID` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `pictures`
---
-ALTER TABLE `pictures`
-  MODIFY `PicID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
-
---
--- AUTO_INCREMENT for table `sponsor`
---
-ALTER TABLE `sponsor`
-  MODIFY `SpoID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
-
---
--- AUTO_INCREMENT for table `ticket`
---
-ALTER TABLE `ticket`
-  MODIFY `TicID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
-
---
--- AUTO_INCREMENT for table `vendor`
---
-ALTER TABLE `vendor`
-  MODIFY `VenID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=63;
+  `VenPicture` longblob,
+  PRIMARY KEY (`VenID`),
+  UNIQUE KEY `VenName` (`VenName`)
+) ENGINE=InnoDB AUTO_INCREMENT=64 DEFAULT CHARSET=latin1;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
